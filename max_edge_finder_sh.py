b511bf6f5ab9bac2daf7694ddd7aff72ea2ac5fa
Eric Ma <ericmajinglong@gmail.com>
2016-04-12 14:59:01 -0400

    modifications to place  at command line

--- max_edge_finder_sh.py
+++ max_edge_finder_sh.py
@@ -1,5 +1,7 @@
-shhandle = '20150902_all_ird'
-pyhandle = shhandle
+import os
+import networkx as nx
+import sys
+
 
 def get_header(n_nodes):
     header = '\
@@ -11,12 +13,7 @@ def get_header(n_nodes):
 #$ -M ericmjl@mit.edu \n\
 #$ -pe whole_nodes {0}\n\
 #############################################\n\n'.format(n_nodes)
-
     return header
-
-import os
-import networkx as nx
-
 
 
 def check_dirs(dirname):
@@ -25,27 +22,36 @@ def check_dirs(dirname):
     else:
         pass
 
-check_dirs('edges')
+if __name__ == '__main__':
 
-G = nx.read_gpickle('{0} Initialized Graph.pkl'.format(pyhandle))
+    shhandle = sys.argv[1]
+    pyhandle = shhandle
 
-batch_size = 1000
+    check_dirs('edges')
+    check_dirs('shell_scripts/max_edge_finder')
 
-for idx in range(0, len(G.nodes()), batch_size):
-    with open('shell_scripts/max_edge_finder/max_edge_finder{0}.sh'.format(idx), 'w+') as f:
-        f.write(get_header(n_nodes=4))
+    G = nx.read_gpickle('{0} Initialized Graph.pkl'.format(pyhandle))
 
-        f.write('cd ..\n')
-        f.write('cd ..\n')
+    batch_size = 1000
 
-        f.write('python max_edge_finder.py {0} {1} {2}'.format(shhandle, idx, idx + batch_size))
-
-with open('shell_scripts/max_edge_finder.sh', 'w') as f:
-    f.write(get_header(n_nodes=1))
-    f.write('cd max_edge_finder\n')
-    
     for idx in range(0, len(G.nodes()), batch_size):
-        
-        f.write('qsub max_edge_finder{0}.sh\n'.format(idx))
-        f.write('sleep 5\n')
-        
+        with open('shell_scripts/max_edge_finder/max_edge_finder{0}.sh'.format(
+                idx), 'w+') as f:
+            f.write(get_header(n_nodes=4))
+
+            f.write('cd ..\n')
+            f.write('cd ..\n')
+
+            f.write('python max_edge_finder.py {0} {1} {2}'.format(shhandle,
+                                                                   idx,
+                                                                   idx +
+                                                                   batch_size))
+
+    with open('shell_scripts/max_edge_finder.sh', 'w') as f:
+        f.write(get_header(n_nodes=1))
+        f.write('cd max_edge_finder\n')
+
+        for idx in range(0, len(G.nodes()), batch_size):
+
+            f.write('qsub max_edge_finder{0}.sh\n'.format(idx))
+            f.write('sleep 5\n')
